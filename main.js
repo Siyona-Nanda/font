1d5d6fa62026b0e30014772e5d0b1300c54a57d7
Siyona-Nanda <88610903+Siyona-Nanda@users.noreply.github.com>
2022-02-07 16:52:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function setup() {
+    canvas = createCanvas(400, 400);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(400, 400);
+    posenet = ml5.poseNet(video, modelLoaded);
+    posenet.on("pose", gotPoses);
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+    }
+}
+
+function modelLoaded() {
+    console.log("posenet is working");
+
+}
+
+function draw() {
+    background("limegreen");
+}
